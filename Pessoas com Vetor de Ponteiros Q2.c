fe7a6f80188933b438769b9278f27819835d8c72
Vladimyr Guedes <123104597+SrVlady@users.noreply.github.com>
2023-02-24 13:15:13 -0300

    Update Pessoas com Vetor de Ponteiros Q2.c

--- Pessoas com Vetor de Ponteiros Q2.c
+++ Pessoas com Vetor de Ponteiros Q2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct pessoa {
+struct pessoa {  //estrutura com os dados de uma pessoa
     char nome[50];
     int idade, num_doc;
 };
 
-void ler(struct pessoa **p, int qnt_p) {
+void ler(struct pessoa **p, int qnt_p) {  //função para ler os dados de uma pessoa
     int i;
 
-    for(i = 0; i < qnt_p; i++) {
+    for(i = 0; i < qnt_p; i++) {           //laço de repetição para receber os dados das pessoas
 
         printf("\nDigite o nome da pessoa %d: ", i + 1);
         scanf(" %[^\n]s", p[i] -> nome);
@@ -22,10 +22,10 @@ void ler(struct pessoa **p, int qnt_p) {
 
 }
 
-void imprime(struct pessoa **p, int qnt_p) {
+void imprime(struct pessoa **p, int qnt_p) {   //função para imprimir os dados das pessoas
     int i;
 
-    for(i = 0; i < qnt_p; i++) {
+    for(i = 0; i < qnt_p; i++) {                //laço de repetição para imprimir os dados das pessoas
 
         printf("\n=======================\n");
         printf("\nPessoa num.%d ", i + 1);
@@ -37,7 +37,7 @@ void imprime(struct pessoa **p, int qnt_p) {
 
 }
 
-void altera(struct pessoa **p) {
+void altera(struct pessoa **p) {    //função para alterar a idade de uma pessoa
     int altera;
     char escolha;
     
@@ -52,35 +52,35 @@ void altera(struct pessoa **p) {
         printf("\nDigite o numero da pessoa que deseja alterar a idade: ");
         scanf("%d", &altera);
         printf("\nDigite a nova idade: ");
-        scanf("%d", &p[altera - 1] -> idade);
+        scanf("%d", &p[altera - 1] -> idade);                    //digita a nova idade da pessoa a partir do numero dessa pessoa - 1 para poder acessar o elemento certo
         printf("\nA pessoa %d agora tem %d anos.", altera, p[altera - 1] -> idade);
 
     }
 
 }
 
-void maior(struct pessoa **p, int qnt_p) {
+void maior(struct pessoa **p, int qnt_p) {   //função para calcular e mostrar quem a pessoa mais velha e a mais nova
     int maior, imaior, menor, imenor, i;
 
     for(i = 0; i < qnt_p; i++) {
 
-        if(i == 0) {
-
-            maior = p[i] -> idade;
+        if(i == 0) {                        //condição que faz com que no começo do laço da repetição, as variáveis maior e menor recebem os valores
+                                            //mas apenas no começo do laço, depois ele irá atualizar minhas variáveis
+            maior = p[i] -> idade;          
             menor = p[i] -> idade;
             imaior = i;
             imenor = i;
 
         }
 
-        if(maior < p[i] -> idade) {
+        if(maior < p[i] -> idade) {       //condição para verificar se a minha variável "maior" é menor que a idade atual que está sendo analizada
 
             maior = p[i] -> idade;
             imaior = i;
 
         }
 
-        if(menor > p[i] -> idade) {
+        if(menor > p[i] -> idade) {       //condição para verificar se a minha variável "menor" é maior que a idade atual que está sendo analizada
 
             menor = p[i] -> idade;
             imenor = i;
@@ -100,19 +100,19 @@ int main(void) {
     printf("Digite a quantidade de pessoas: ");
     scanf("%d", &qnt_pessoas);
 
-    struct pessoa **p = (struct pessoa**) malloc(qnt_pessoas * sizeof(struct pessoa*));
+    struct pessoa **p = (struct pessoa**) malloc(qnt_pessoas * sizeof(struct pessoa*));  //alocando minha variável p dinamicamente
 
     for(i = 0; i < qnt_pessoas; i++) {
 
         p[i] = (struct pessoa*) malloc(qnt_pessoas * sizeof(struct pessoa*));
     }
 
-    ler(p, qnt_pessoas);
+    ler(p, qnt_pessoas);            //chamada das funções
     imprime(p, qnt_pessoas);
     altera(p);
     maior(p, qnt_pessoas);
 
-    for(i = 0; i < qnt_pessoas; i++) {
+    for(i = 0; i < qnt_pessoas; i++) {    //liberando minha memória
 
         free(p[i]);
     }
