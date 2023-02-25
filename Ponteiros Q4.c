5c81250be0a4446cb688f925e025a7c03829cdda
Vladimyr Guedes <123104597+SrVlady@users.noreply.github.com>
2023-02-25 15:10:31 -0300

    Update Ponteiros Q4.c

--- Ponteiros Q4.c
+++ Ponteiros Q4.c
@@ -4,16 +4,16 @@
 void calcula_hexagono(float l, float *area, float *perimetro);
 
 int main(void) {
-    float l, area, perimetro, *pa, *pp;
+    float l, area, perimetro, *pa, *pp;                        
     pa = &area;
     pp = &perimetro;
-    scanf("%f", &l);
-    calcula_hexagono(l, pa, pp);
+    scanf("%f", &l);                                             
+    calcula_hexagono(l, pa, pp);                                //chamada da função passando como parametro os ponteiros
     printf("Area = %.2f\nPerimetro = %.2f", area, perimetro);
     return(0);
 }
 
-void calcula_hexagono(float l, float *area, float *perimetro) {
+void calcula_hexagono(float l, float *area, float *perimetro) { //função que vai calcular a área do perímetro 
     *area = 3 * ((pow(l, 2)) * (sqrt(3))) / 2;
     *perimetro = 6 * l;
 }
